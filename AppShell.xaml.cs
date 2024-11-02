4e6d67ddf7e7fe979ac44b78f903d6c86c906c91
cawairodrigo <cawairodrigojj@gmail.com>
2024-11-02 14:00:22 -0300

    Adicionar arquivos de projeto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TryndadeMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TryndadeMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
